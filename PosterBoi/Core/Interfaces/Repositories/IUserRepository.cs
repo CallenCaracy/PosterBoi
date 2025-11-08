b4c6599e3b42445e0d30f9c55438dea52fea4de3
CallenCaracy <diangcocalen@gmail.com>
2025-11-09 00:07:43 +0800

    Jwt and repository setup for auth and user done halfway

---
+++ PosterBoi/Core/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using PosterBoi.Core.Models;
+
+namespace PosterBoi.Core.Interfaces.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByEmailAsync(string email);
+        Task<User?> GetByIdAsync(Guid id);
+        Task<bool> CreateUserAsync(User user);
+    }
+}
