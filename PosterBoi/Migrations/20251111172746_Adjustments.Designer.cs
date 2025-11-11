ca4076f96127b479862aeea6e29969f108725a73
CallenCaracy <diangcocalen@gmail.com>
2025-11-12 01:28:19 +0800

    Migrations adjustment

--- PosterBoi/Migrations/20251111172746_Adjustments.Designer.cs
+++ PosterBoi/Migrations/20251111172746_Adjustments.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PosterBoi.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using PosterBoi.Infrastructure.Data;
 namespace PosterBoi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251111172746_Adjustments")]
+    partial class Adjustments
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
