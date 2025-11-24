a3b53a56a905282e6847a37faa1662e1514afedb
CallenCaracy <diangcocalen@gmail.com>
2025-11-24 15:58:21 +0800

    Cors adjustment

---
+++ PosterBoi/API/Controllers/v1/HealthCheckController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PosterBoi.API.Controllers.v1
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("/api/v{version:apiVersion}/[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet("healthCheck")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        public IActionResult HealthCheck()
+        {
+            return Ok("Healthy");
+        }
+    }
+}
